27eeb8b47103598b131e8b0cdd6d1e11dc2ab2c0
MathVerg <matheo.vergnolle@polytechnique.edu>
2020-08-14 11:24:49 +0200

    Si verrou bloqué, on demande si l'on veut poursuivre au lieu de bloquer

--- EBM.py
+++ EBM.py
@@ -37,7 +37,7 @@ class EBMMain(Tk):
         """ Constructeur """
         # Tk.__init__(self)
         super().__init__()
-        
+
         #pour bien fermer la base de données avant de quitter
         self.protocol("WM_DELETE_WINDOW", self.quitter)
 
@@ -60,10 +60,10 @@ class EBMMain(Tk):
         #self.bouton_export.pack(side=LEFT)
         self.bouton_quitter = Button(self.cadre_boutons, text="Quitter", command=self.quitter)
         self.bouton_quitter.pack(side=LEFT)
-        
+
         self.cadre_cv = Frame(self)
         self.cadre_cv.pack(fill=BOTH, expand=True, padx=5, pady=5)
-        
+
         self.cv = Canvas(self.cadre_cv, bg='white', scrollregion=(0, 0, w_cv, h_cv))
         self.cv.focus_set()
         self.cv.bind('<Button-1>', self.clic)
@@ -78,25 +78,25 @@ class EBMMain(Tk):
         self.scroll_x.pack(side=BOTTOM, fill=X)
         self.cv.pack(side = LEFT, fill=BOTH, expand=True, padx=5, pady=5)
         self.cv.configure(yscrollcommand=self.scroll_y.set, xscrollcommand=self.scroll_x.set)
-        
+
         self.bind("<MouseWheel>", self.mouse_scroll)
         self.bind("<Button-4>", self.mouse_scroll)
         self.bind("<Button-5>", self.mouse_scroll)
-        
+
         self.infobulle_texte = None
         self.infobulle_cadre = None
-        
+
         self.max_resa = 0
         self.selection = 0
         self.secure = True
-        
+
 
     def initialise_tableau(self):
-        
+
         #coloriage des emplacements pairs
         for k in range(2, nb_places+1, 2):
            self.cv.create_rectangle(1 * w_case, (4 + k) * h_case, 66 * w_case, (5 + k) * h_case, fill=fond_place_pair)
-        
+
         #jours
         for k in range(1, 32):
             jour = date(annee, 7, k).weekday()
@@ -113,7 +113,7 @@ class EBMMain(Tk):
             self.cv.create_text(int((33.5 + k) * w_case), int(3.5 * h_case), text=str(k))
             self.cv.create_text(int((33.5 + k) * w_case), int((5.5 + nb_places)* h_case), text=str(k))
             self.cv.create_text(int((33.5 + k) * w_case), int(4.5 * h_case), text=rep_jours[jour])
-        
+
         #lignes maîtresses horizontales
         self.cv.create_line(2 * w_case, h_case, 65 * w_case, h_case, width=l_forte)
         self.cv.create_line(2 * w_case, 2 * h_case, 65 * w_case, 2 * h_case, width=l_forte)
@@ -126,7 +126,7 @@ class EBMMain(Tk):
         self.cv.create_line(2 * w_case, (6 + nb_places) * h_case, 65 * w_case, (6 + nb_places) * h_case, width=l_forte)
         self.cv.create_line(2 * w_case, (7 + nb_places) * h_case, 65 * w_case, (7 + nb_places) * h_case, width=l_forte)
         self.cv.create_line(2 * w_case, (7 + nb_places + prof_pool) * h_case, 65 * w_case, (7 + nb_places + prof_pool) * h_case, width=l_forte)
-        
+
         #lignes maîtresses verticales
         self.cv.create_line(w_case, 5 * h_case, w_case, (5 + nb_places) * h_case, width=l_forte)
         self.cv.create_line(2 * w_case, h_case, 2 * w_case, (7 + nb_places + prof_pool) * h_case, width=l_forte)
@@ -136,24 +136,24 @@ class EBMMain(Tk):
         self.cv.create_line(34 * w_case, (7 + nb_places) * h_case, 34 * w_case, (7 + nb_places + prof_pool) * h_case, width=l_forte)
         self.cv.create_line(65 * w_case, h_case, 65 * w_case, (7 + nb_places + prof_pool) * h_case, width=l_forte)
         self.cv.create_line(66 * w_case, 5 * h_case, 66 * w_case, (5 + nb_places) * h_case, width=l_forte)
-        
+
         #cadrillage horizontal
         for k in range(1, nb_places):
             self.cv.create_line(w_case, (5 + k) * h_case, 65 * w_case, (5 + k) * h_case, width=l_faible)
         for k in range(1, prof_pool):
             self.cv.create_line(2 * w_case, (7 + nb_places + k) * h_case, 65 * w_case, (7 + nb_places + k) * h_case, width=l_faible)
-            
+
         #cadrillage vertical
         for k in range(1, 63):
             self.cv.create_line((2 + k) * w_case, 3 * h_case, (2 + k) * w_case, (6 + nb_places) * h_case, width=l_faible)
             self.cv.create_line((2 + k) * w_case, (7 + nb_places) * h_case, (2 + k) * w_case, (7 + nb_places + prof_pool) * h_case, width=l_faible)
-        
+
         #titres et mois
         self.cv.create_text(int(33.5 * w_case), int(1.5 * h_case), text="P l a n n i n g  " + str(annee) + "  a u  " + str(date.today().day)+" / " + str(date.today().month) + " / " + str(date.today().year))
         self.cv.create_text(int(17.5 * w_case), int(2.5 * h_case), text="J u i l l e t")
         self.cv.create_text(int(49.5 * w_case), int(2.5 * h_case), text="A o u t")
         self.cv.create_text(int(33.5 * w_case), int((6.5 + nb_places) * h_case), text="P i s c i n e")
-        
+
         #numéros des emplacements
         for k in range(1, nb_places + 1):
             self.cv.create_text(int(1.5 * w_case), int((4.5 + k) * h_case), text=str(k))
@@ -186,8 +186,7 @@ class EBMMain(Tk):
                 verrou = int(f.readline())
                 f.close()
                 if verrou :
-                    mb.showerror("Erreur", "Un autre utilisateur modifie cette base de données en ce moment")
-                    flag = False
+                    flag = mb.askyesno("Attention", "Un autre utilisateur pourrait être en train de modifier cette base de données. Voulez-vous tout de même continuer ?")
             except FileNotFoundError :
                 flag = mb.askyesno("Attention", "Aucun fichier de verrou n'a été trouvé : nous ne pouvons pas garantir que vous êtes en ce moment le/la seule.e à modifier cette base de données. Voulez-vous tout de même continuer ?")
                 if flag :
@@ -225,7 +224,7 @@ class EBMMain(Tk):
                         self.placer_resa(row)
                     #self.bouton_export['state'] = NORMAL
             self.cv.focus_set()
-    
+
     def placer_resa(self, row):
         (num_resa, nomAffiche, arrivee, depart, frigo, ombre, emplacement, ancre) = row
         mois_arrivee = int(arrivee[1])
@@ -270,7 +269,7 @@ class EBMMain(Tk):
         self.cv.itemconfig(self.liste_rectangles[num_resa], outline = bordure, width=2)
         self.liste_noms[num_resa] = self.cv.create_text((x0 + x1)//2, y0 - 1 + int(h_case/2), text=texte_affiche)
         self.liste_places[num_resa] = emplacement
-            
+
     def nelle_resa(self):
         param = (self.max_resa + 1, "", "", "07/01", "07/02", 0, 0, 0, 2, 0, "", 0, 0)
         edition_resa = EditionResa(self, param)
@@ -286,7 +285,7 @@ class EBMMain(Tk):
             self.cur.execute('''INSERT INTO Reservations (NumeroResa, Nom, NomAffiche, Arrivee, Depart, Couchage, Frigo, Ombre, Adultes, Enfants, Divers, Emplacement, Ancre) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)''', valeurs)
             self.apres_modif()
         self.cv.focus_set()
-        
+
     def infos_resa(self):
         self.cur.execute('''SELECT * FROM Reservations WHERE NumeroResa = ?''', (self.selection,))
         param = self.cur.fetchone()
@@ -303,13 +302,13 @@ class EBMMain(Tk):
             self.apres_modif()
             self.selectionne(self.selection)
         self.cv.focus_set()
-        
+
     def export_planning(self):
         filepath = fd.asksaveasfilename(title = "Enregistrer l'image")
         self.selectionne(0)
         self.cv.postscript(file=filepath + ".ps", colormode='color')
         self.cv.focus_set()
-        
+
     def suppr_resa(self):
         if mb.askokcancel(title="Confirmation", message="Voulez-vous vraiment supprimer cette réservation ?"):
             self.cv.delete(self.liste_rectangles[self.selection])
@@ -329,14 +328,14 @@ class EBMMain(Tk):
                 self.liste_places[self.selection] = None
             self.selectionne(0)
             self.cv.focus_set()
-        
+
     def enregistrer(self):
         self.db.commit()
         self.bouton_enregistrer['state'] = DISABLED
         #self.bouton_export['state'] = NORMAL
         self.bouton_fichier_db['state'] = NORMAL
         self.cv.focus_set()
-        
+
     def quitter(self):
         quit_flag = True
         try :
@@ -366,13 +365,13 @@ class EBMMain(Tk):
             except AttributeError:
                 pass
             self.destroy()
-    
+
     def apres_modif(self):
         '''actions effectuées après chaque modification'''
         self.bouton_enregistrer['state'] = NORMAL
         #self.bouton_export['state'] = DISABLED
         self.bouton_fichier_db['state'] = DISABLED
-    
+
     def selectionne(self, i):
         if 0 < self.selection <= self.max_resa :
             self.cur.execute('''SELECT Ancre FROM Reservations WHERE NumeroResa = ?''', (self.selection,))
@@ -394,12 +393,12 @@ class EBMMain(Tk):
             self.bouton_infos_resa['state'] = DISABLED
             self.bouton_suppr_resa['state'] = DISABLED
             self.cv.focus()
-    
+
     def check_conflits(self, num, arrivee, depart, place):
         self.cur.execute('''SELECT COUNT(*) FROM Reservations WHERE Emplacement=? AND (Depart > ? OR Arrivee > ? ) AND (Depart < ? OR Arrivee < ?) AND NumeroResa <> ?''', (place, arrivee, arrivee, depart, depart, num))
         conflits = self.cur.fetchone()[0]
         return conflits
-        
+
     def clic(self, event):
         ''' ce qui se passe lorsqu'on clique quelque part sur le canevas '''
         X, Y = event.x + self.scroll_x.get()[0] * w_cv, event.y + self.scroll_y.get()[0] * h_cv #on compense le scroll
@@ -431,7 +430,7 @@ class EBMMain(Tk):
             offset_bas = (2 + int(bool(couchage)) + int(bool(divers)))*16 + 3
             self.infobulle_cadre = self.cv.create_rectangle(X-3, Y-3, X + max(90, len(nom) * 9, len(divers) * 8), Y + offset_bas, fill='yellow')
             self.infobulle_texte = self.cv.create_text(X, Y, text=texte_info, anchor=NW)
-            
+
     def drag(self, event):
         Y = event.y + self.scroll_y.get()[0] * h_cv #on compense le scroll
         flag = True
@@ -459,14 +458,14 @@ class EBMMain(Tk):
             self.cv.tag_raise(self.infobulle_cadre)
             self.cv.tag_raise(self.infobulle_texte)
             self.selectionne(self.selection)
-        
+
     def release(self, event):
         if self.selection != 0 :
             self.cur.execute('''SELECT Emplacement FROM Reservations WHERE NumeroResa = ?''', (self.selection,))
             if self.cur.fetchone()[0] != self.liste_places[self.selection]:
                 self.cur.execute('''UPDATE Reservations SET Emplacement = ? WHERE NumeroResa = ?''', (self.liste_places[self.selection], self.selection))
                 self.apres_modif()
-                    
+
     def touche_dir(self, dir):
         '''dir = +- 1 indique la direction de déplacement'''
         if self.selection == 0 :
@@ -509,42 +508,42 @@ class EBMMain(Tk):
                 self.cur.execute('''UPDATE Reservations SET Emplacement = ? WHERE NumeroResa = ?''', (new_place, row[0]))
                 self.apres_modif()
                 self.selectionne(self.selection)
-    
+
     def touche_haut(self, event):
         self.touche_dir(-1)
-    
+
     def touche_bas(self, event):
         self.touche_dir(1)
-        
+
     def mouse_scroll(self, event):
         if event.num == 5 or event.delta < 0:
-            dir = 1 
+            dir = 1
         else :
             dir = -1
         self.cv.yview_scroll(dir, "units")
-              
+
 class EditionResa(Toplevel):
     '''fenetre d'édition des réservations'''
     def __init__(self, parent, row):
         '''row (tuple), contient les valeurs par défaut on renverra une liste à la fin'''
-        
+
         Toplevel.__init__(self, parent)
-        
+
         self.parent = parent
         self.transient(self.parent)
         self.grab_set()
-        
+
         #pour révoquer les modifs avant de quitter
         self.protocol("WM_DELETE_WINDOW", self.annuler)
-        
+
         self.title("Édition de réservation")
-        
+
         self.valeurs_origine = list(row)
         self.valide = True
-        
+
         self.grand_cadre = Frame(self, padx=5, pady=5, relief=GROOVE)
         self.grand_cadre.pack()
-        
+
         self.panneau_gauche = Frame(self.grand_cadre, padx=5, pady=5, relief=SUNKEN)
         self.panneau_gauche.pack(side=LEFT)
 
@@ -553,13 +552,13 @@ class EditionResa(Toplevel):
         self.nom = StringVar(self, value=row[1])
         self.champ_nom = Entry(self.cadre_nom, textvariable=self.nom, width=60)
         self.champ_nom.pack()
-        
+
         self.cadre_nom_affiche = LabelFrame(self.panneau_gauche, text="Nom à afficher", padx=5, pady=5, relief=GROOVE)
         self.cadre_nom_affiche.pack()
         self.nom_affiche = StringVar(self, value=row[2])
         self.champ_nom_affiche = Entry(self.cadre_nom_affiche, textvariable=self.nom_affiche, width=60)
         self.champ_nom_affiche.pack()
-        
+
         self.cadre_arrivee = LabelFrame(self.panneau_gauche, text="Arrivée", padx=5, pady=5, relief=GROOVE)
         self.cadre_arrivee.pack()
         arrivee = row[3]
@@ -569,7 +568,7 @@ class EditionResa(Toplevel):
         self.choix_jour_a.pack(side=LEFT)
         self.choix_mois_a = Scale(self.cadre_arrivee,from_=6,to=8,resolution=1,orient=HORIZONTAL, length=50, width=20, label="Mois", tickinterval=2, variable=self.mois_arrivee)
         self.choix_mois_a.pack(side=LEFT)
-        
+
         self.cadre_depart = LabelFrame(self.panneau_gauche, text="Départ", padx=5, pady=5, relief=GROOVE)
         self.cadre_depart.pack()
         depart = row[4]
@@ -579,16 +578,16 @@ class EditionResa(Toplevel):
         self.choix_jour_d.pack(side=LEFT)
         self.choix_mois_d = Scale(self.cadre_depart,from_=7,to=9,resolution=1,orient=HORIZONTAL, length=50, width=20, label="Mois", tickinterval=2, variable=self.mois_depart)
         self.choix_mois_d.pack(side=LEFT)
-        
+
         self.cadre_divers = LabelFrame(self.panneau_gauche, text="Divers", padx=5, pady=5, relief=GROOVE)
         self.cadre_divers.pack()
         self.divers = StringVar(self, value=row[10])
         self.champ_divers = Entry(self.cadre_divers, textvariable=self.divers, width=60)
         self.champ_divers.pack()
-        
+
         self.panneau_droite = Frame(self.grand_cadre, padx=5, pady=5, relief=SUNKEN)
         self.panneau_droite.pack(side=LEFT)
-        
+
         self.cadre_couchage = LabelFrame(self.panneau_droite, text="Couchage", padx=5, pady=5, relief=GROOVE)
         self.cadre_couchage.pack()
         self.couchage = IntVar(self, value=row[5])
@@ -598,7 +597,7 @@ class EditionResa(Toplevel):
         self.choix_caravane.pack(side=LEFT)
         self.choix_campingcar = Radiobutton(self.cadre_couchage, text="Camping-car", variable=self.couchage, value=2)
         self.choix_campingcar.pack(side=LEFT)
-        
+
         self.cadre_options = LabelFrame(self.panneau_droite, text="Options", padx=5, pady=5, relief=GROOVE)
         self.cadre_options.pack()
         self.frigo = IntVar(self, value=row[6])
@@ -607,7 +606,7 @@ class EditionResa(Toplevel):
         self.ombre = IntVar(self, value=row[7])
         self.case_ombre = Checkbutton(self.cadre_options, text="Ombre", variable=self.ombre)
         self.case_ombre.pack(side=LEFT)
-        
+
         self.cadre_famille = LabelFrame(self.panneau_droite, text="Composition de la famille", padx=5, pady=5, relief=GROOVE)
         self.cadre_famille.pack()
         self.adultes = IntVar(self, value=row[8])
@@ -617,7 +616,7 @@ class EditionResa(Toplevel):
         self.enfants = IntVar(self, value=row[9])
         self.choix_enfants = Scale(self.cadre_famille, from_=0, to=9, resolution=1, orient=HORIZONTAL, length=350, width=20, label="Nombre d'enfants :", tickinterval=1, variable=self.enfants)
         self.choix_enfants.pack()
-        
+
         self.cadre_emplacement = LabelFrame(self.panneau_droite, text="Emplacement", padx=5, pady=5, relief=GROOVE)
         self.cadre_emplacement.pack()
         self.emplacement = IntVar(self, value=row[11])
@@ -626,21 +625,21 @@ class EditionResa(Toplevel):
         self.ancre = IntVar(self, value=row[12])
         self.case_ancre = Checkbutton(self.cadre_emplacement, text="Ancré à l'emplacment", variable=self.ancre)
         self.case_ancre.pack()
-        
+
         self.cadre_vc = Frame(self, borderwidth=5, relief=GROOVE)
         self.cadre_vc.pack()
         self.bouton_valider = Button(self.cadre_vc, text="Valider", command=self.valider)
         self.bouton_valider.pack(side=LEFT)
         self.bouton_annuler = Button(self.cadre_vc, text="Annuler", command=self.annuler)
         self.bouton_annuler.pack(side=LEFT)
-    
+
     def annuler(self):
         self.adultes.get()
         self.valeurs = self.valeurs_origine
         self.valide = False
         self.parent.focus_set()
         self.destroy()
-    
+
     def valider(self):
         arrivee = "0{0}/{1:02}".format(self.mois_arrivee.get(), self.jour_arrivee.get())
         depart = "0{0}/{1:02}".format(self.mois_depart.get(), self.jour_depart.get())
@@ -668,8 +667,8 @@ class EditionResa(Toplevel):
             self.valide = True
             self.parent.focus_set()
             self.destroy()
-        
-                
+
+
 
 if __name__ == "__main__":
     princ = EBMMain()
